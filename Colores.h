11fc39052adb5cc93192cb949b36f17df2f71cfe
kevin-Maca <54913146+kevin-Maca@users.noreply.github.com>
2025-04-01 16:36:34 -0500

    Create Colores.h
    Archivo para modificar el color de las cartas

---
+++ Colores.h
@@ -0,0 +1,25 @@
+#ifndef COLORES_H
+#define COLORES_H
+
+// Definiciones de colores de fondo
+#define  BG_BLACK    "\x1B[40m"
+#define  BG_RED      "\x1B[41m"
+#define  BG_GREEN    "\x1B[42m"
+#define  BG_YELLOW   "\x1B[43m"
+#define  BG_BLUE     "\x1B[44m"
+#define  BG_MAGENTA "\x1B[45m"
+#define  BG_CYAN     "\x1B[46m"
+#define  BG_WHITE    "\x1B[47m"
+#define  BG_NARANJA   "\x1B[48;2;255;128;0m"
+#define  BG_LBLUE    "\x1B[48;2;53;149;240m"
+#define  BG_LGREEN   "\x1B[48;2;17;245;120m"
+#define  BG_GRAY     "\x1B[48;2;176;174;174m"
+#define  BG_ROSE     "\x1B[48;2;255;151;203m"
+
+// Definición de color de texto blanco
+#define WHITE       "\x1B[37m"
+
+// Reset para el color
+#define RESET       "\x1b[0m"
+
+#endif 
